6baaf3b720b701f424cd771f10441d81d0fa325f
CuongTV <46061394+cuonganh@users.noreply.github.com>
2022-08-15 10:50:37 +0700

    init Employee Transfer service

---
+++ src/main/java/com/example/employeetransfer/EmployeeTransferApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeetransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTransferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTransferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeetransfer/EmployeeTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeetransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
